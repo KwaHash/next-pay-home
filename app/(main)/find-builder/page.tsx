93c706e17686bc0927675230c5610ccc6f2ea0a9
StackNinja0109 <whitecrow0109@gmail.com>
2025-01-21 06:21:32 +0900

    feat(builder): add find-builder page, theme and selectbox with mui

--- app/(main)/find-builder/page.tsx
+++ app/(main)/find-builder/page.tsx
@@ -1,7 +1,9 @@
+import FindBuilderPage from "@/features/FindBuilderPage";
+
 export default function FindBuilder() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-between mt-20">
-
+    <main className="flex flex-col flex-grow items-center justify-between mt-20">
+      <FindBuilderPage />
     </main>
   );
 }
